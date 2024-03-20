8c9aaf0f9bd1a1d3cd3552bc6bc5c143072a27a7
ourfor <ourfor@qq.com>
2024-03-20 21:34:04 +0800

    ✨ add player ui

--- ios/PlayerManager.h
+++ ios/PlayerManager.h
@@ -11,8 +11,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface Player : RCTViewManager
-@property (nonatomic, strong) VLCMediaPlayer *player;
+@interface PlayerManager : RCTViewManager
+
 @end
 
 NS_ASSUME_NONNULL_END
