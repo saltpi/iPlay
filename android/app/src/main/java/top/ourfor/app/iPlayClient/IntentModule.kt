0325cd23661ca041b8248165d272c1de435e8a9e
ourfor <ourfor@qq.com>
2024-03-23 22:25:50 +0800

    💩 add bad code

--- android/app/src/main/java/top/ourfor/app/iPlayClient/IntentModule.kt
+++ android/app/src/main/java/top/ourfor/app/iPlayClient/IntentModule.kt
@@ -3,6 +3,7 @@ package top.ourfor.app.iPlayClient
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -24,5 +25,15 @@ class IntentModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
         } catch (e: ActivityNotFoundException) {
             Log.d("IntentModule", e.toString())
         }
+    }
+
+    @ReactMethod
+    fun playFile(filepath: String) {
+        val intent = Intent()
+        intent.putExtra("filepath", filepath)
+        val context = BeanManager.get<Context>(Context::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setClassName(context, "top.ourfor.app.iPlayClient.mpv.MPVActivity")
+        context.startActivity(intent)
     }
 }
