61fd7525c68fdb21ac23a70a2878d6a858f74155
ourfor <ourfor@qq.com>
2024-04-04 10:22:06 +0800

    💚 fix Xcode Cloud build

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,7 @@
 brew install cocoapods
+brew install node
 
 # Install JS dependencies
-cd /Volumes/workspace/repository && yarn
+cd /Volumes/workspace/repository && npm i
 # Install pods under the ios folder of your react native project
 cd /Volumes/workspace/repository/ios && pod install
