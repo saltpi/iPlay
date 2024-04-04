5e1203248bda30b40a7220403721bc4672843176
ourfor <ourfor@qq.com>
2024-04-04 10:16:39 +0800

    💚 add Xcode cloud script

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,6 @@
+brew install cocoapods
+
+# Install JS dependencies
+cd /Volumes/workspace/repository && yarn
+# Install pods under the ios folder of your react native project
+cd /Volumes/workspace/repository/ios && pod install
